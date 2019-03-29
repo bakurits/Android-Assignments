f301ddb37cca47404eb0e46ebb846133118e673e
bakurits <bakuricucxashvili@gmail.com>
2019-03-29 11:45:12 +0400

    Layout finished

--- Assignment1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Assignment1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,12 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.billionCount);
+        textView.setText("1");
 
 
 
