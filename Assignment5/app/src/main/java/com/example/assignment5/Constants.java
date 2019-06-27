545b280247b753bd1e759a757d32dff896551f69
bakurits <bakuricucxashvili@gmail.com>
2019-06-27 13:36:35 +0400

    Some changes

---
+++ Assignment5/app/src/main/java/com/example/assignment5/Constants.java
@@ -0,0 +1,17 @@
+package com.example.assignment5;
+
+class Constants {
+
+    static final int FOREGROUND_SERVICE = 101;
+
+
+    static final String NOTIFICATION_CHANNEL_ID = "com.example.assignment5.notification_chanel_id";
+    static final String START_FOREGROUND_ACTION = "com.example.assignment5.start_foreground_action";
+    static final String INIT_FOREGROUND_ACTION = "com.example.assignment5.init_foreground_action";
+    static final String STOP_FOREGROUND_ACTION = "com.example.assignment5.stop_foreground_action";
+    static final String MAIN_ACTION = "com.example.assignment5.action.main";
+    static final String PREV_ACTION = "com.example.assignment5.action.prev";
+    static final String PLAY_ACTION = "com.example.assignment5.action.play";
+    static final String NEXT_ACTION = "com.example.assignment5.action.next";
+
+}
